f4fb1056de7d76126208af06d686c990ffc663cf
volokhinn <csgomx@mail.ru>
2023-08-03 23:05:31 +0700

    Added WelcomeScreen

--- App.js
+++ App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import { StyleSheet, Platform, SafeAreaView } from 'react-native';
 import AppNavigation from './navigation/AppNavigation';
 import { StatusBar } from 'expo-status-bar';
+import * as NavigationBar from 'expo-navigation-bar';
+import { useFonts } from 'expo-font';
+import { ActivityIndicator } from 'react-native';
+
+NavigationBar.setBackgroundColorAsync('#000');
 
 export default function App() {
+  let [fontLoaded] = useFonts({
+    Inter400: require('./assets/fonts/Inter-Regular.ttf'),
+    Inter100: require('./assets/fonts/Inter-ExtraLight.ttf'),
+  });
+
+  if (!fontLoaded) {
+    <ActivityIndicator size={20} color="black" />;
+  }
+
   return (
     <>
-      <StatusBar style="dark" />
-      <SafeAreaView className="flex-1" style={styles.AndroidSafeArea}>
-        <AppNavigation />
-      </SafeAreaView>
+      <StatusBar style="light" />
+      {/* <SafeAreaView className="flex-1" style={styles.AndroidSafeArea}> */}
+      <AppNavigation />
+      {/* </SafeAreaView> */}
     </>
   );
 }
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,54 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  TouchableHighlight,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
+import { ArrowRightIcon } from 'react-native-heroicons/outline';
+import { LinearGradient } from 'expo-linear-gradient';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function WelcomeScreen() {
+export default function WelcomeScreen({ navigation }) {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-black text-4xl">HomeScreen</Text>
+    <View>
+      <ImageBackground
+        className="h-full w-full"
+        resizeMode="cover"
+        source={require('../assets/img/welcomebg.png')}>
+        <View className="flex-1 relative">
+          <Image className="mt-12 ml-[-150]" source={require('../assets/img/coin.png')} />
+          <View className="mx-2 mt-12">
+            <Text className="text-white font-bold text-[24px]">MoneyFlow</Text>
+            <Text className="text-white font-black text-[48px] mt-2">
+              Your Personal Banking App
+            </Text>
+            <Text className="text-white font-normal text-[16px] mt-2">
+              Manage finances, pay bills, transfer funds, track expenses securely and easily from
+              your mobile device.
+            </Text>
+            <LinearGradient
+              colors={['rgba(255, 255, 255, 0.5)', 'rgba(0, 0, 0, 0.00)']}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              style={{ padding: 1 }}
+              className="rounded-full mt-10">
+              <TouchableOpacity
+                onPress={() => navigation.navigate('Home')}
+                className="flex-row justify-between items-center rounded-full p-4"
+                style={{ backgroundColor: 'rgba(0,0,0,0.8)' }}>
+                <Text className="text-white text-2xl uppercase font-semibold tracking-[2px]">
+                  Start free trial
+                </Text>
+                <ArrowRightIcon size={25} color={'white'} />
+              </TouchableOpacity>
+            </LinearGradient>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
